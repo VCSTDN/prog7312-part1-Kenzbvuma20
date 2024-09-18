73fa8545f0f1aed8484439da3f336b6dbfe7ecec
Kenzbvuma <104380963+Kenzbvuma@users.noreply.github.com>
2024-09-18 20:55:16 +0200

    Fixed progress bar

--- ReportsIssue/obj/Debug/net8.0/ReportsIssue.AssemblyInfo.cs
+++ ReportsIssue/obj/Debug/net8.0/ReportsIssue.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReportsIssue")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9edb5426c0d63918ab57dda8853e06d16b343076")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReportsIssue")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReportsIssue")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
